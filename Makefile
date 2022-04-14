5aa35c3c8f9d91d3ccb88d348fc85ae326ea6b1b
Milot Mirdita <milot@mirdita.de>
2022-04-14 15:58:16 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,9 @@
+all: tmalign-wasm.js tmalign-wasm.wasm
+
+SMALL:=-Oz -flto -s EVAL_CTORS=1
+
+tmalign-wasm.js tmalign-wasm.wasm: TMalign.cpp
+	emcc TMalign.cpp -std=c++11 ${SMALL} -fno-rtti -s EXPORTED_RUNTIME_METHODS=callMain,FS -s INVOKE_RUN=0 -s FILESYSTEM=1 -s ENVIRONMENT=web -s MODULARIZE=1 -s EXPORT_ES6=1 -s 'EXPORT_NAME="createTmalign"' -s SINGLE_FILE=0 -s ASSERTIONS=0 -o tmalign-wasm.js
+
+clean:
+	rm -f tmalign-wasm.js tmalign-wasm.wasm
---
+++ tmalign.js
@@ -0,0 +1,27 @@
+import { default as createTmalign } from './tmalign-wasm.js'
+//import tmalignWasm from './tmalign-wasm.wasm'
+
+function tmalign(pdb1, pdb2) {
+    return new Promise((resolve, reject) => {
+        let buffer = "";
+        createTmalign({
+            //locateFile: () => tmalignWasm,
+            print: (msg) => buffer += msg + "\n"
+        }).then((instance) => {
+            instance.FS.writeFile('/pdb1.pdb', pdb1);
+            instance.FS.writeFile('/pdb2.pdb', pdb2);
+            const err = instance.callMain([
+                "/pdb1.pdb",
+                "/pdb2.pdb",
+            ]);
+            if (err == 0) {
+                resolve(buffer)
+            } else {
+                reject(err)
+            }
+        });
+    })
+}
+
+export { tmalign };
+
